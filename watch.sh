a89633f9756427f75127b1653068e64d39d695f1
Robert Lemke <robert@flownative.com>
2019-03-08 13:56:00 +0100

    Fix for filenames containing spaces

--- watch.sh
+++ watch.sh
@@ -8,31 +8,27 @@ while true
 do
     (ls /data/input | grep '.pdf$' | grep -v '^--') | \
         while read filename; do
-            pathAndFilename=/data/input/${filename}
+            pathAndFilename="/data/input/${filename}"
             fileExtension="${pathAndFilename##*.}"
 
-            if [[ "$fileExtension" == "pdf" ]]; then
-                echo "Starting ocr for ${pathAndFilename}"
+            echo "Starting ocr for ${pathAndFilename}"
 
-                filenamePrefix=$(echo ${filename} | grep -oE [^._-]+ | head -1)
+            filenamePrefix=$(echo ${filename} | grep -oE [^._-]+ | head -1)
 
-                ocrmypdf -l deu+eng --sidecar /tmp/ocr/${filename}.txt ${pathAndFilename} /tmp/ocr/${filename}.pdf
+            ocrmypdf -l deu+eng --sidecar "/tmp/ocr/${filename}.txt" "${pathAndFilename}" "/tmp/ocr/${filename}.pdf"
 
-                documentNumber=$(head -n 1 /tmp/ocr/${filename}.txt | grep -E ^\s*[0-9]{7}\s*$)
-                if [[ -z "${documentNumber}" ]]; then
-                    targetFilename=${filename}
-                else
-                    targetFilename=${filenamePrefix}-${documentNumber}.pdf
-                fi
-
-                mv -f /tmp/ocr/${filename}.pdf /data/output/${targetFilename}
-                mv ${pathAndFilename} /data/input/--${filename}
-                rm /tmp/ocr/${filename}.txt
-
-                echo "Finished ocr, result is ${targetFilename}"
+            documentNumber=$(head -n 1 /tmp/ocr/${filename}.txt | grep -E ^\s*[0-9]{7}\s*$)
+            if [[ -z "${documentNumber}" ]]; then
+                targetFilename=${filename}
             else
-                echo "Ignoring ${pathAndFilename}: not a PDF file"
+                targetFilename=${filenamePrefix}-${documentNumber}.pdf
             fi
+
+            mv -f /tmp/ocr/${filename}.pdf /data/output/${targetFilename}
+            mv ${pathAndFilename} /data/input/--${filename}
+            rm /tmp/ocr/${filename}.txt
+
+            echo "Finished ocr, result is ${targetFilename}"
         done
     sleep 5
     echo -ne "."
