b5fd0d100f448628c2da3a09f9cb79e3146a3588
Robert Lemke <robert@flownative.com>
2019-03-08 13:12:06 +0100

    Replace inotify by regular polling

--- watch.sh
+++ watch.sh
@@ -4,33 +4,38 @@ mkdir -p /tmp/ocr
 
 echo "Starting OCR watcher ..."
 
-(inotifywait -m /data/input --format '%w%f' -e create) | \
-    while read pathAndFilename; do
-        fileExtension="${pathAndFilename##*.}"
+while true
+do
+    (find /data/input -regex '.*\/[^-]*\.pdf') | \
+        while read pathAndFilename; do
+            fileExtension="${pathAndFilename##*.}"
 
-        if [[ "$fileExtension" == "pdf" ]]; then
-            echo "Starting ocr for ${pathAndFilename}"
+            if [[ "$fileExtension" == "pdf" ]]; then
+                echo "Starting ocr for ${pathAndFilename}"
 
-            filename=$(basename ${pathAndFilename})
-            filenamePrefix=$(echo ${filename} | grep -oE [^._-]+ | head -1)
+                filename=$(basename ${pathAndFilename})
+                filenamePrefix=$(echo ${filename} | grep -oE [^._-]+ | head -1)
 
-            ocrmypdf -l deu+eng --sidecar /tmp/ocr/${filename}.txt ${pathAndFilename} /tmp/ocr/${filename}.pdf
+                ocrmypdf -l deu+eng --sidecar /tmp/ocr/${filename}.txt ${pathAndFilename} /tmp/ocr/${filename}.pdf
 
-            documentNumber=$(head -n 1 /tmp/ocr/${filename}.txt | grep -E ^\s*[0-9]{7}\s*$)
-            if [[ -z "${documentNumber}" ]]; then
-                targetFilename=${filename}
+                documentNumber=$(head -n 1 /tmp/ocr/${filename}.txt | grep -E ^\s*[0-9]{7}\s*$)
+                if [[ -z "${documentNumber}" ]]; then
+                    targetFilename=${filename}
+                else
+                    targetFilename=${filenamePrefix}-${documentNumber}.pdf
+                fi
+
+                mv -f /tmp/ocr/${filename}.pdf /data/output/${targetFilename}
+                mv ${pathAndFilename} /data/input/--${filename}
+                rm /tmp/ocr/${filename}.txt
+
+                echo "Finished ocr, result is ${targetFilename}"
             else
-                targetFilename=${filenamePrefix}-${documentNumber}.pdf
+                echo "Ignoring ${pathAndFilename}: not a PDF file"
             fi
-
-            mv -f /tmp/ocr/${filename}.pdf /data/output/${targetFilename}
-            mv ${pathAndFilename} /data/input/--${filename}
-            rm /tmp/ocr/${filename}.txt
-
-            echo "Finished ocr, result is ${targetFilename}"
-        else
-            echo "Ignoring ${pathAndFilename}: not a PDF file"
-        fi
-    done
+        done
+    sleep 5
+    echo -ne "."
+done
 
 echo "OCR watcher exited."
